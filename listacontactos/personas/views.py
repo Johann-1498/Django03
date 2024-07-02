ed7e5dc940a0bcbda18ed3d963326c4432389ff7
Johann-1498 <jcaceresruiz@unsa.edu.pe>
2024-07-01 23:06:03 -0500

    Renderizacion de una Persona mediante un diccionario y tambien mediante un objeto

--- listacontactos/personas/views.py
+++ listacontactos/personas/views.py
@@ -1,4 +1,11 @@
 from django.shortcuts import render
 from django.http import HttpResponse
-
+from .models import Persona
 # Create your views here.
+def personaTestView(request):
+    obj = Persona.objects.get(id=1)
+    context ={
+        'nombre': obj.nombre,
+        'edad': obj.edad,
+    }
+    return render(request, 'personas/test.html', context)
