cfd8580ad5ea37fb6fa33439608d7175f5ef0249
Johann-1498 <jcaceresruiz@unsa.edu.pe>
2024-07-01 22:44:09 -0500

    Implementacion el contexto en vistas de app inicio con la creación de un diccionario y recibiendolo en home.html

---
+++ listacontactos/inicio/views.py
@@ -0,0 +1,13 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+# Create your views here.
+def myHomeView(request, *args, **kwargs):
+    myContext = {
+        'myText': 'Esto es sobre nosotros',
+        'myNumber': 123,
+        'myList': [33, 44, 55]
+    }
+    return render(request, 'home.html', myContext)
+
+def anotherView(request, *args, **kwargs):
+    return HttpResponse('<h1>Solo otra pagina</h1>')
